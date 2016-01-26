1f134a4e34ba1353a15e7858e3893af1b9b1e49d
Simon Werner <simonwerner@gmail.com>
2016-01-27 10:51:19 +1300

    Added test runner for remote proxy

--- test/runAll.sh
+++ test/runAll.sh
@@ -14,4 +14,12 @@ node testWRC.js
 echo "Web-Remote-Control - TCP"
 PROTOCOL=TCP node testWRC.js
 
+# The following tests require a proxy configured and running on a remote server.
+#
+# echo "Web-Remote-Control - UDP - Using Remote Proxy"
+# PROXY_ADDRESS="my.remote.server" node testWRC.js
+#
+# echo "Web-Remote-Control - TCP - Using Remote Proxy"
+# PROXY_ADDRESS="my.remote.server" PROTOCOL=TCP node testWRC.js
+
 cd ..
